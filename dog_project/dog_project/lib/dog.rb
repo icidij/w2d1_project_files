8c140caadacfcae1314e8db5f0d79652741aaf66
icidij <jamesc1288@gmail.com>
2021-08-16 13:58:22 -0400

    add dog project spec files

--- dog_project/dog_project/lib/dog.rb
+++ dog_project/dog_project/lib/dog.rb
@@ -1,5 +1,48 @@
 class Dog
-    def initialize(name, breed, agec)
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+
+        @breed = breed
+
+        @age = age
+        @bark = bark
+
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    
+    end
+
+
+    def breed
+        @breed
+
+    end
+
+    def age
+
+        @age
+    end
+
+    def bark
+
+        if @age > 3
+
+            bark.uppercase
+        else
+            bark.lowercase
+            
+        end
+    end
+
+
+    def age=(new_num)
+
+        @age = new_num
+
+    end
 
 
 end
